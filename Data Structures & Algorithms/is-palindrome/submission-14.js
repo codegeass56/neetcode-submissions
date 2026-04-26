8ebb6e3c24f644bf947eb6c5f4ffbae557d2e312
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2026-04-26 23:13:17 +0400

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+        while(left < right) {
+            while(left < right && !(/[A-Za-z0-9]/.test(s[left]))) {
+                left++;
+            }
+            while(right > left && !(/[A-Za-z0-9]/.test(s[right]))) {
+                right--;
+            }
+            if(s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
