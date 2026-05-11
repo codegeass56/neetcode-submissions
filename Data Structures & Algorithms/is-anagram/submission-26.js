b2850fd6ceaeb4fa7d039e7919b743b06ef0cd67
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2026-05-11 14:31:04 +0400

    Add: is-anagram - submission-26

---
+++ Data Structures & Algorithms/is-anagram/submission-26.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        if(s === t) return true;
+
+        const mapS = {};
+        const mapT = {};
+
+        for(const c of s) {
+            mapS[c] = (mapS[c] ?? 0) + 1;
+        }
+
+        for(const c of t) {
+            mapT[c] = (mapT[c] ?? 0) + 1;
+        }
+
+        for(const k of Object.keys(mapS)) {
+            if(mapT[k] !== mapS[k]) return false;
+        }
+
+        return true;
+    }
+}
