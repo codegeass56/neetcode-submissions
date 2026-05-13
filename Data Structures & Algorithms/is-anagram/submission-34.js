9d299982db26e82dd9305d83ef82c208cd7a4824
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2026-05-13 14:58:36 +0400

    Add: is-anagram - submission-34

---
+++ Data Structures & Algorithms/is-anagram/submission-34.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        if(s === t) return true;
+
+        const sSort = s.split('').sort().join();
+        const tSort = t.split('').sort().join();
+        return sSort === tSort;
+    }
+}
