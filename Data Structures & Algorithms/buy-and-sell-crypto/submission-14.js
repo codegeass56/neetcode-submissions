b09456fd6f1fa0884c06c8e40fa9b7a32e07de44
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2026-05-14 15:32:57 +0400

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minBuyingPrice = prices[0];
+        let maxProfit = 0;
+
+        for(let i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minBuyingPrice);
+            minBuyingPrice = Math.min(minBuyingPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
