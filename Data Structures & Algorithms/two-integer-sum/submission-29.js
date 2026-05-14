032bf3776a282563976b6145c53fa3bcae2bcaae
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2026-05-14 14:27:17 +0400

    Add: two-integer-sum - submission-29

---
+++ Data Structures & Algorithms/two-integer-sum/submission-29.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    //hashmap two pass
+    //hashmap single pass
+    //using sorting with an array of pairs
+    twoSum(nums, target) {
+        const map = {};
+        for(let i = 0; i < nums.length; i++) {
+            map[nums[i]] = i;
+        }
+
+        for(let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if(map[diff] !== undefined && map[diff] !== i) {
+                return [i, map[diff]];
+            }
+        }
+
+        return [];
+    }
+}
